9652c362b9a736170b50fc0f099fbde2cca55033
ryeon-xx <rlarkgus3415@gmail.com>
2026-04-18 13:12:52 +0900

    Chapter 01 - 자바 시작하기

---
+++ src/Chap01/sec09/Hello.java
@@ -0,0 +1,9 @@
+package Chap01.sec09;
+
+public class Hello {
+    // main + tab 자동완성
+    public static void main(String[] args) {
+        // sout + tab 자동완성
+        System.out.println("Hello, Java");
+    }
+}
---
+++ src/Chap01/verify/Example.java
@@ -0,0 +1,7 @@
+package Chap01.verify;
+
+public class Example {
+    public static void main(String[] args) {
+        System.out.println("개발자가 되기 위한 필수 개발 언어 Java");
+    }
+}
